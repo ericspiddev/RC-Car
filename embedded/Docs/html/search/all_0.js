0161b7bd9b0bd5d11210db8d2d3db00a1cbe420e
MSI\zacly <zaclynn26@gmail.com>
2022-06-01 16:14:28 -0400

    C documentation

---
+++ embedded/Docs/html/search/all_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['app_2ec_0',['app.c',['../app_8c.html',1,'']]],
+  ['app_2eh_1',['app.h',['../app_8h.html',1,'']]],
+  ['app_5finit_2',['app_init',['../app_8c.html#add3190cf715f513666f4be42874d91e2',1,'app_init(void):&#160;app.c'],['../app_8h.html#add3190cf715f513666f4be42874d91e2',1,'app_init(void):&#160;app.c']]],
+  ['app_5fprocess_5faction_3',['app_process_action',['../app_8c.html#a62b9d027d612c675bc9ae3c81102763f',1,'app_process_action(void):&#160;app.c'],['../app_8h.html#a62b9d027d612c675bc9ae3c81102763f',1,'app_process_action(void):&#160;app.c']]]
+];
---
+++ embedded/Docs/html/search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pwm_2ec_0',['PWM.c',['../_p_w_m_8c.html',1,'']]]
+];
