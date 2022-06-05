f824c18ca3e7687b36c25f02320e6e284def8728
Gage Elenbaas <elenbaag@mail.gvsu.edu>
2022-06-05 16:48:52 -0400

    Function additions, motor testing
    Added prototypes, initial functions to test, servo movement, added control

--- embedded/RC_BLE/RC_BLE.ino
+++ embedded/RC_BLE/RC_BLE.ino
@@ -5,7 +5,7 @@
 
    Create a BLE server that, once we receive a connection, will send periodic notifications.
    The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
    Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
 
    The design of creating the BLE server is:
@@ -17,7 +17,7 @@
    6. Start advertising.
 
    In this example rxValue is the data received (only accessible inside that function).
-   And txValue is the data to be sent, in this example just a byte incremented every second. 
+   And txValue is the data to be sent, in this example just a byte incremented every second.
 */
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -25,7 +25,6 @@
 #include <BLE2902.h>
 #include <ESP32PWM.h>
 #include <ESP32Servo.h>
-
 
 
 BLEServer *pServer = NULL;
@@ -41,6 +40,15 @@ uint8_t txValue = 0;
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
+void setRCSpeed();
+void setRCDirection();
+void setRCFR();
+
+Servo steer;
+const int forwardLeft = 25; //PWM
+const int forwardRight = 14; //PWM
+const int reverseLeft = 26; //PWM
+const int reverseRight = 27; //PWM
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
@@ -84,16 +92,16 @@ void setup() {
 
   // Create a BLE Characteristic
   pTxCharacteristic = pService->createCharacteristic(
-										CHARACTERISTIC_UUID_TX,
-										BLECharacteristic::PROPERTY_NOTIFY
-									);
-                      
+                        CHARACTERISTIC_UUID_TX,
+                        BLECharacteristic::PROPERTY_NOTIFY
+                      );
+
   pTxCharacteristic->addDescriptor(new BLE2902());
 
   BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
-											 CHARACTERISTIC_UUID_RX,
-											BLECharacteristic::PROPERTY_WRITE
-										);
+      CHARACTERISTIC_UUID_RX,
+      BLECharacteristic::PROPERTY_WRITE
+                                          );
 
   pRxCharacteristic->setCallbacks(new MyCallbacks());
 
@@ -103,27 +111,77 @@ void setup() {
   // Start advertising
   pServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
+
+  steer.attach(32); //GPIO 32 FOR SERVO MOVEMENT
+
+
+  ledcAttachPin(reverseLeft,0);
+  ledcAttachPin(reverseRight,0);
+  ledcAttachPin(forwardRight,0);
+  ledcAttachPin(forwardLeft,0);
+
+  ledcWrite(reverseLeft,0);
+  ledcWrite(reverseRight,0);
+  ledcWrite(forwardRight,0);
+  ledcWrite(forwardLeft,0);
+
+
+  // Initialize channels
+  // channels 0-15, resolution 1-16 bits, freq limits depend on resolution
+  // ledcSetup(uint8_t channel, uint32_t freq, uint8_t resolution_bits);
+  ledcSetup(0, 4000, 8); // 12 kHz PWM, 8-bit resolution
+
 }
 
 void loop() {
 
-    if (deviceConnected) {
-        pTxCharacteristic->setValue(&txValue, 1);
-        pTxCharacteristic->notify();
-        txValue++;
-		delay(10); // bluetooth stack will go into congestion, if too many packets are sent
-	}
+  if (deviceConnected) {
+    pTxCharacteristic->setValue(&txValue, 1);
+    pTxCharacteristic->notify();
+    txValue++;
+    delay(10); // bluetooth stack will go into congestion, if too many packets are sent
+  }
 
-    // disconnecting
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    // connecting
-    if (deviceConnected && !oldDeviceConnected) {
-		// do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
-    }
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising(); // restart advertising
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+    oldDeviceConnected = deviceConnected;
+  }
+
+  setRCDirection(180);
+
+}
+
+void setRCDirection(int directionVar) {s
+  if (directionVar == 0) { //straight
+    steer.write(0);
+  }
+  else if (directionVar == 1) { //left
+    steer.write(90);
+  }
+  else if () { //right
+    steer.write(180);
+  }
+}
+
+void setRCFR(int FR, int speedVar) {
+  if (FR == 1) {
+    ledcWrite(forwardLeft, speedVar);
+    ledcWrite(forwardRight, speedVar);
+    ledcWrite(reverseLeft, 0);
+    ledcWrite(reverseRight, 0);
+  }
+  else {
+    ledcWrite(reverseLeft, speedVar);
+    ledcWrite(reverseRight, speedVar);
+    ledcWrite(forwardLeft, 0);
+    ledcWrite(forwardRight, 0ar);
+  }
 }
