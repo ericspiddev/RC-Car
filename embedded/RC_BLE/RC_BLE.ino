966f63efbc736498a0502db138795ddf5ad4f664
Zac Lynn <zaclynn26@gmail.com>
2022-06-18 13:12:49 -0400

    Comments added

--- embedded/RC_BLE/RC_BLE.ino
+++ embedded/RC_BLE/RC_BLE.ino
@@ -1,31 +1,18 @@
-/*
-   Video: https://www.youtube.com/watch?v=oCMOYS71NIU
-   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
-   Ported to Arduino ESP32 by Evandro Copercini
-
-  Create a BLE server that, once we receive a connection, will send periodic notifications.
-  The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
-  Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE"
-  Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
-
-  The design of creating the BLE server is:
-  1. Create a BLE Server
-  2. Create a BLE Service
-  3. Create a BLE Characteristic on the Service
-  4. Create a BLE Descriptor on the characteristic
-  5. Start the service.
-  6. Start advertising.
-
-  In this example rxValue is the data received (only accessible inside that function).
-  And txValue is the data to be sent, in this example just a byte incremented every second.
-*/
+/*******************************************************************************
+ * @file RC_BLE.ino
+ * @author Gage Elenbaas, Aaron Mulder, Eric Spidle, Zac Lynn, Daniel Brillhart
+ * @brief This code contains functions to initialize a BT connection with the 
+ *          Android app, and to drive the DC motors. 
+ * @version 2
+ * @date 2022-06-18
+ * 
+ ******************************************************************************/
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <sstream>
 #include <ESP32Servo.h>
-#include <AUnit.h>
 
 BLEServer *pServer = NULL;
 BLECharacteristic * pTxCharacteristic;
@@ -43,63 +30,76 @@ std:: string hold;
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
-#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+// UART service UUID
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
-void decodeCommand (uint8_t command);
+void decodeCommand(uint8_t command);
 void setRCDirection(int directionVar, int speedVar);
 void setRCFR(int FR, int speedVar);
 
-// the number of the motor pin
-const int forLeft = 16;  // 16 corresponds to GPIO16
-const int revLeft = 14; // 14 corresponds to GPIO14
-const int forRight = 26; // 26 corresponds to GPIO26 
-const int revRight = 27; // 27 corresponds to GPIO27 //reverse right
+// The number of the motor pin
+const int forLeft = 16;     // 16 corresponds to GPIO16
+const int revLeft = 14;     // 14 corresponds to GPIO14
+const int forRight = 26;    // 26 corresponds to GPIO26
+const int revRight = 27;    // 27 corresponds to GPIO27 //reverse right
 
-// setting PWM properties
-const int freq = 50; //100hz frequency
+// Setting PWM properties
+const int freq = 50;        // 100hz frequency
 const int leftForwardChan = 5;
 const int leftReverseChan = 6;
 const int rightForwardChan = 4;
 const int rightReverseChan = 3;
-const int resolution = 8; //8 bit resolution
+const int resolution = 8;   // 8 bit resolution
 
-//delay designation
+// Delay designation
 const int delayms = 1000;
 
-/*
 
-*/
 class MyServerCallbacks: public BLEServerCallbacks {
+    /***************************************************************************
+     * @brief This function is called whenever a BT device connects.
+     * 
+     * @param pServer GAT server pointer
+     **************************************************************************/
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
-    };
+    }
 
+    /***************************************************************************
+     * @brief This function is called when a BT device disconnects.
+     * 
+     * @param pServer 
+     **************************************************************************/
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
     }
 };
 
-/*
 
-*/
 class MyCallbacks: public BLECharacteristicCallbacks {
+    /***************************************************************************
+     * @brief When data is written to RX characteristic, collet data, convert 
+     *              to int and output
+     * 
+     * @param pCharacteristic BLE pointer to Rx characteristic
+     **************************************************************************/
     void onWrite(BLECharacteristic *pCharacteristic) {
       std::string rxValue = pCharacteristic->getValue();
       Serial.println("data recived");
       std:: stringstream convert(rxValue);
       convert >> command;
       Serial.println("data converted to int");
-
     }
 };
 
-/*
-
-*/
+/*******************************************************************************
+ * @brief This function initializes four PWM pins to drive the motors and 
+ *          sets up the BT connection.
+ ******************************************************************************/
 void setup() {
-  // configure LED PWM functionalitites
+  // Configure LED PWM functionalitites
   delay(delayms);
   ledcSetup(leftReverseChan, freq, resolution);
   delay(delayms);
@@ -110,7 +110,7 @@ void setup() {
   ledcSetup(rightReverseChan, freq, resolution);
   delay(delayms);
 
-  // attach the channel to the GPIO to be controlled
+  // Attach the channel to the GPIO to be controlled
   ledcAttachPin(forLeft, leftForwardChan);
   delay(delayms);
   ledcAttachPin(revLeft, leftReverseChan);
@@ -133,15 +133,13 @@ void setup() {
   // Create a BLE Characteristic
   pTxCharacteristic = pService->createCharacteristic(
                         CHARACTERISTIC_UUID_TX,
-                        BLECharacteristic::PROPERTY_NOTIFY
-                      );
+                        BLECharacteristic::PROPERTY_NOTIFY);
 
   pTxCharacteristic->addDescriptor(new BLE2902());
 
   BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
       CHARACTERISTIC_UUID_RX,
-      BLECharacteristic::PROPERTY_WRITE
-                                          );
+      BLECharacteristic::PROPERTY_WRITE);
 
   pRxCharacteristic->setCallbacks(new MyCallbacks());
 
@@ -158,11 +156,12 @@ void setup() {
   Serial.println("Waiting a client connection to notify...");
 }
 
-/*
-
-*/
+/*******************************************************************************
+ * @brief If a BT connection has been made and the command data has changed 
+ *          the loop updates motor speed and direction. If no BT connection
+ *          has been made, start advertising and try to connect to a device.
+ ******************************************************************************/
 void loop() {
-
   if (deviceConnected && command != oldCommand) {
     //        pTxCharacteristic->setValue(&txValue, 1);
     //        pTxCharacteristic->notify();
@@ -178,37 +177,39 @@ void loop() {
     Serial.println(setCarSpeed);
     setRCDirection(steering, setCarSpeed);
     setRCFR(setDirection, setCarSpeed);
-    delay(10); // bluetooth stack will go into congestion, if too many packets are sent
+    // Bluetooth stack will go into congestion, if too many packets are sent
+    delay(10);
   }
 
-  // disconnecting
+  // Disconnecting
   if (!deviceConnected && oldDeviceConnected) {
-    delay(500); // give the bluetooth stack the chance to get things ready
-    pServer->startAdvertising(); // restart advertising
+    // Give the bluetooth stack the chance to get things ready
+    delay(500);
+    pServer->startAdvertising();        // restart advertising
     Serial.println("start advertising");
     oldDeviceConnected = deviceConnected;
   }
-  // connecting
+  // Connecting
   if (deviceConnected && !oldDeviceConnected) {
-    // do stuff here on connecting
+    // Do stuff here on connecting
     oldDeviceConnected = deviceConnected;
   }
-  aunit::TestRunner::run();
 }
 
-/*
-
-*/
+/*******************************************************************************
+ * @brief This Function turns the RC car left or right.
+ * 
+ * @param directionVar Flag to give direction (2 = left, 1 = right)
+ * @param speedVar Used to set motor speed
+ ******************************************************************************/
 void setRCDirection(int directionVar, int speedVar) {
-  if (directionVar == 0) { //stop
-  }
-  else if (directionVar == 2) { //left
+  if (directionVar == 0) {              // Stop
+  } else if (directionVar == 2) {       // Left
     ledcWrite(leftForwardChan, 0);
     ledcWrite(rightForwardChan, speedVar);
     ledcWrite(leftReverseChan, speedVar);
     ledcWrite(rightReverseChan, 0);
-  }
-  else if (directionVar == 1) { //right
+  } else if (directionVar == 1) {       // Right
     ledcWrite(leftForwardChan, speedVar);
     ledcWrite(rightForwardChan, 0);
     ledcWrite(leftReverseChan, 0);
@@ -216,18 +217,20 @@ void setRCDirection(int directionVar, int speedVar) {
   }
 }
 
-/*
-
-*/
+/*******************************************************************************
+ * @brief This function sets the RC car motors to drive forwards or reverse.
+ * 
+ * @param FR Flag that gives direction (1 = forward, 2 = reverse)
+ * @param speedVar Used to set motor speed
+ ******************************************************************************/
 void setRCFR(int FR, int speedVar) {
-  speedVar = speedVar * 15; //multiples to reach 255 resolution
+  speedVar = speedVar * 15;             // Multiples to reach 255 resolution
   if (FR == 1) {
     ledcWrite(leftForwardChan, speedVar);
     ledcWrite(rightForwardChan, speedVar);
     ledcWrite(leftReverseChan, 0);
     ledcWrite(rightReverseChan, 0);
-  }
-  else {
+  } else {
     ledcWrite(leftReverseChan, speedVar);
     ledcWrite(rightReverseChan, speedVar);
     ledcWrite(rightForwardChan, 0);
@@ -235,11 +238,13 @@ void setRCFR(int FR, int speedVar) {
   }
 }
 
-/*
-
-*/
-void decodeCommand (uint8_t command)
-{
+/*******************************************************************************
+ * @brief This function decodes the hex command received over BT into a 
+ *          steering variable, speed variable and direction variable. 
+ * 
+ * @param command 8-bit hex command from BT connection
+ *******************************************************************************/
+void decodeCommand(uint8_t command) {
   byte maskSteer = B00110000;
   byte maskDirect = B11000000;
   byte maskSpeed = B00001111;
@@ -247,5 +252,4 @@ void decodeCommand (uint8_t command)
   steering = (command & maskSteer) >> 4;
   setDirection = (command & maskDirect) >> 6;
   setCarSpeed = (command & maskSpeed);
-
 }
